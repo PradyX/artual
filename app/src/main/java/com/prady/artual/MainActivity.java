5c13f2211ad5f1b8fc17c90e2be73f7b6b1f66bb
PradyX <pradyumnbhushan@gmail.com>
2020-05-18 21:04:28 +0530

    add bottomAppBar nav items
    Signed-off-by: PradyX <pradyumnbhushan@gmail.com>

--- app/src/main/java/com/prady/artual/MainActivity.java
+++ app/src/main/java/com/prady/artual/MainActivity.java
@@ -1,14 +1,53 @@
 package com.prady.artual;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomAppBar bottomAppBar;
+    private CoordinatorLayout coordinatorLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coordinatorLayout = findViewById(R.id.coordinator_main);
+        bottomAppBar = findViewById(R.id.bottom_bar);
+        setSupportActionBar(bottomAppBar);
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(coordinatorLayout, "Menu", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.bottom_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.bottom_search:
+                Snackbar.make(coordinatorLayout, "Search", Snackbar.LENGTH_LONG).show();
+                break;
+            case R.id.bottom_settings:
+                Snackbar.make(coordinatorLayout, "Settings", Snackbar.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
