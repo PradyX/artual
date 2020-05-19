df758a7c3593ac563909455e5f8a32dc5208d0be
PradyX <pradyumnbhushan@gmail.com>
2020-05-19 23:52:25 +0530

    add dummy stuff for HomeHragment
    Signed-off-by: PradyX <pradyumnbhushan@gmail.com>

--- app/src/main/java/com/prady/artual/fragments/HomeFragment.java
+++ app/src/main/java/com/prady/artual/fragments/HomeFragment.java
@@ -16,11 +16,11 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
 
         return root;
     }
